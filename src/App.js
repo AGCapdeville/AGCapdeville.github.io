aaff058984e58f1fa4d53fedc7f94cbc52701818
Adam <agcapdeville@gmail.com>
2019-10-27 02:02:13 -0700

    photo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src="https://i.imgur.com/eDmARH5.jpg" class="img-circle"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
