3013540524b201d16f157386d97b3f4138d887fb
Adam <agcapdeville@gmail.com>
2019-10-27 00:20:09 -0700

    Updates

---
+++ precache-manifest.6ab7360c8ee4c60cca79bb34c7be33df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86a61aece6936167fed4ed56f4062c6c",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "8931fae1f5468d16a548",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3db6c6e2b9e1d4597b89",
+    "url": "/react-gh-pages/static/js/2.946ea795.chunk.js"
+  },
+  {
+    "revision": "8931fae1f5468d16a548",
+    "url": "/react-gh-pages/static/js/main.c10e29a0.chunk.js"
+  },
+  {
+    "revision": "d1ce432b67083ccc0594",
+    "url": "/react-gh-pages/static/js/runtime-main.460020dd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
