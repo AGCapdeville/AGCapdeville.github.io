774ad3b6298ec07b6d72e0a47b72097dd8f699c9
Adam <agcapdeville@gmail.com>
2019-10-28 16:03:19 -0700

    Footer.js correction

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import App from './Footer';
+import Footer from './Footer';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
