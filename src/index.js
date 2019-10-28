1e148404f77a26b1374ac4f045e600c5c802ea97
Adam <agcapdeville@gmail.com>
2019-10-28 16:02:25 -0700

    alter index.js for Footer.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from './Footer';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Footer />,document.getElementById('root'));
