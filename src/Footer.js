42e5c95b54f75582e6358144972ae652354bbace
Adam <agcapdeville@gmail.com>
2019-10-28 15:59:38 -0700

    Footer component added

---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import logo from './logo.svg';
+
+function Footer() {
+  return (    
+    <div>
+      <footer>
+        <p>
+          "Adam Capdeville 2019 | "
+          <a href="https://www.linkedin.com/in/adamcapdeville/"
+          >Linkedin
+          </a>;
+          " | "
+          <a 
+            className="App-link"
+            href="https://github.com/AGCapdeville/AGCapdeville.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <b>GitHub</b> Pages Repe 
+          </a>;
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
