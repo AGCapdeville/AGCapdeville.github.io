123a8c75ce1ab79fa9c953b64833d2cdabaefed3
Adam <agcapdeville@gmail.com>
2019-11-01 10:38:11 -0700

    Refactored JS, CSS & React

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import NavigationBar from './NavigationBar.js';
-import Footer from './Footer.js';
+import Footer from './Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar/>
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src="https://i.imgur.com/eDmARH5.jpg" class="img-circle"/>
-        <p>
-          Adam Capdeville
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/AGCapdeville/AGCapdeville.github.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <b>GitHub</b> Pages Repo
-        </a>
-      </header>
+    <body>
+      <NavigationBar />
       <Footer />
-    </div>
+    </body>
   );
 }
 
--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -1,21 +1,71 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import NotFound from './NotFound';
+import Portfolio from './Portfolio';
+import Blog from './Blog';
+import Home from './Home';
 
-function NavigationBar() {
-  return (  
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#HOME">Adam Capdeville</Nav.Link>
-            <Nav.Link href="portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="blog">Blog</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+class NavigationBar extends React.Component{
+  render () {  
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul class="nav-ul navbar-custom">
+              <li>
+                <NavLink
+                  to="/Home"
+                  activeClassName="selected"
+                > Home 
+                </NavLink>             
+              </li>
+              <li>
+                <NavLink
+                  to="/Portfolio"
+                  activeClassName="selected"
+                > Portfolio 
+                </NavLink> 
+              </li >
+              <li>
+                <NavLink
+                  to="/Blog"
+                  activeClassName="selected"
+                > Blog 
+                </NavLink>               
+              </li>
+              <li>
+                <a
+                className="App-link App-left"
+                href="https://github.com/AGCapdeville/AGCapdeville.github.io"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <b>GitHub</b> Pages Repo
+                </a>
+              </li>
+            </ul>
+          </nav>
+            
+          <Switch>
+            <Router path="/Home"> <Home /> </Router>
+            <Router path="/Portfolio"> <Portfolio /> </Router>
+            <Router path="/Blog"> <Blog /> </Router>
+            <Router component={NotFound}/> 
+          </Switch>
+
+          </div>
+        </Router> 
+      )
+  }
 }
 
 export default NavigationBar;
