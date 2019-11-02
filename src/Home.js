641e7cad48f69f57834e095db32c973b68ffd535
Adam <agcapdeville@gmail.com>
2019-11-02 16:48:52 -0700

    minor grammer change

--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ class Home extends React.Component {
                     </div>
                     <div className='App-description'>
                         <p>
-                            Currently I am a computer science student at CSUCI 
+                            Currently, I am a computer science student at CSUCI 
                             who has a passion for intelligent code, whether it's 
                             intelligently written or written to exhibit intelligence. 
                         </p>
