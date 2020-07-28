ba181a05ca5bf89c9ed70b5c9fd39f7ea69e4593
Adam Capdeville <agcapdeville@gmail.com>
2020-07-28 11:16:26 -0700

    Updates

---
+++ precache-manifest.ad3fc8aa3be9bf351aba857b9c0a9638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22262141c4160b2fb24bbf58b94ac4e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e150d323befd863b803",
+    "url": "/static/css/main.938734ec.chunk.css"
+  },
+  {
+    "revision": "7782eb0647fc92b54b19",
+    "url": "/static/js/2.62bff7cb.chunk.js"
+  },
+  {
+    "revision": "2e0b94b74d604a77ce8bd28c426e1291",
+    "url": "/static/js/2.62bff7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e150d323befd863b803",
+    "url": "/static/js/main.88277769.chunk.js"
+  },
+  {
+    "revision": "a12785feb8678b03a473",
+    "url": "/static/js/runtime-main.c29e4640.js"
+  },
+  {
+    "revision": "0c06f27ea6f33bdf3ac6dae64b19d9f6",
+    "url": "/static/media/daruma.0c06f27e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.958fb2b0557dccc0168184755b4e57a6.js"
+  "/precache-manifest.ad3fc8aa3be9bf351aba857b9c0a9638.js"
 );
 
 self.addEventListener('message', (event) => {
